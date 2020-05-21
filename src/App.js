a8398afba4cbb36fe9c8e18f14881a17bfbc3fa1
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-05-21 14:45:41 -0700

    <AWS S3 BUCKET>:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { UserForm } from './Components/UserForm';
 
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <UserForm/>
+  
       
     </div>
   );
